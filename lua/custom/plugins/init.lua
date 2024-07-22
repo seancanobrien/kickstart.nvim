454171b1b6a651774c44685f7138695ebc1a8cda
Sean O'Brien <seancanobrien@hotmail.co.uk>
2024-07-22 22:54:57 +0100

    Added auto session manager

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -17,6 +17,19 @@ return {
     end,
   },
 
+  -- Session manager
+  {
+    'rmagatti/auto-session',
+    dependencies = {
+      'nvim-telescope/telescope.nvim', -- Only needed if you want to use sesssion lens
+    },
+    config = function()
+      require('auto-session').setup {
+        auto_session_suppress_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+      }
+    end,
+  },
+
   -- Adds custom dictionary and ignore functionality to ltex
   { 'barreiroleo/ltex-extra.nvim' },
 
