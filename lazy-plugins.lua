261c805ebda5d1f3b881edf12c37133c7c0af840
Sean O'Brien <seancanobrien@hotmail.co.uk>
2024-07-25 11:14:16 +0100

    Added nvim-tree option

--- lazy-plugins.lua
+++ lazy-plugins.lua
@@ -62,6 +62,7 @@ require('lazy').setup({
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
+  -- require 'kickstart.plugins.nvim-tree',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
