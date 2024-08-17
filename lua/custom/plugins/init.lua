c7f48ca013b1056ce9c1d7c5737c1b54d841e6e3
Sean O'Brien <seancanobrien@hotmail.co.uk>
2024-08-17 17:54:31 +0100

    Added build directory to vimtex and ultisnips snippets

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -9,12 +9,17 @@ return {
     lazy = false, -- we don't want to lazy load VimTeX
     -- tag = "v2.15", -- uncomment to pin to a specific release
     init = function()
-      -- VimTeX configuration goes here, e.g.
       vim.g.vimtex_view_method = 'zathura'
-      vim.g.vimtex_compiler_latexrun = {
-        build_dir = 'build',
+      vim.g.vimtex_compiler_latexmk = {
+        -- Directory for output pdf and auxilary files
+        out_dir = 'build',
       }
     end,
+  },
+
+  -- Snippets
+  {
+    'SirVer/ultisnips',
   },
 
   -- Session manager
