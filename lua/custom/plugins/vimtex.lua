a83eaf36f25d127915d2e79d84f5dd5aa1ef4223
Sean O'Brien <seancanobrien@hotmail.co.uk>
2024-10-24 12:15:22 +0100

    Make vimtex quickfix info screen go away after keystrokes

--- lua/custom/plugins/vimtex.lua
+++ lua/custom/plugins/vimtex.lua
@@ -4,6 +4,8 @@ return {
     'lervag/vimtex',
     lazy = false, -- we don't want to lazy load VimTeX
     init = function()
+      -- makes the quickfix window go away after any keystrokes
+      vim.g.vimtex_quickfix_autoclose_after_keystrokes = 1
       vim.g.vimtex_view_method = 'zathura'
       vim.g.vimtex_compiler_latexmk = {
         -- Directory for output pdf and auxilary files
