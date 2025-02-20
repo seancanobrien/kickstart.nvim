074c3e8da10c6a24c34c48a6536f5536041eba1b
Sean O'Brien <seancanobrien@hotmail.co.uk>
2025-02-20 18:20:47 +0000

    Changes to vimtex zathura viewer that works better in wayland

--- lua/custom/plugins/vimtex.lua
+++ lua/custom/plugins/vimtex.lua
@@ -6,7 +6,7 @@ return {
     init = function()
       -- makes the quickfix window go away after any keystrokes
       vim.g.vimtex_quickfix_autoclose_after_keystrokes = 1
-      vim.g.vimtex_view_method = 'zathura'
+      vim.g.vimtex_view_method = 'zathura_simple'
       vim.g.vimtex_compiler_latexmk = {
         -- Directory for output pdf and auxilary files
         out_dir = 'build',
