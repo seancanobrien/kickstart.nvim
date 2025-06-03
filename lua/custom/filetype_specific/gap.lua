e920d7f6a0f47b80dd2fac1a8a431e0ef8a840cd
Sean O'Brien <seancanobrien@hotmail.co.uk>
2025-06-03 17:25:18 +0100

    changed gap filetype pattern

--- lua/custom/filetype_specific/gap.lua
+++ lua/custom/filetype_specific/gap.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
-  pattern = { '*.g', '*.gi', '*.gd' },
+  pattern = { '*.g' },
   callback = function()
     vim.bo.filetype = 'gap'
   end,
