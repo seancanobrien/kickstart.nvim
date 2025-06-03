15a7b2105e94c7c43e2fc8bdf8f30869261f4c03
Sean O'Brien <seancanobrien@hotmail.co.uk>
2025-06-03 18:25:17 +0100

    Changed copilot plugin and keybindings. Got rid of copilot chat

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,3 +1,27 @@
+-- Copilot completion.
 return {
-  'github/copilot.vim',
+  {
+    'zbirenbaum/copilot.lua',
+    event = 'InsertEnter',
+    opts = {
+      -- The panel is useless.
+      panel = { enabled = false },
+      suggestion = {
+        auto_trigger = false,
+        hide_during_completion = false,
+        keymap = {
+          accept = '<C-.>',
+          accept_word = '<M-w>',
+          accept_line = '<M-l>',
+          next = '<C-j>',
+          prev = '<C-k>',
+          dismiss = '<C-/>',
+        },
+      },
+      filetypes = {
+        markdown = true,
+        yaml = true,
+      },
+    },
+  },
 }
