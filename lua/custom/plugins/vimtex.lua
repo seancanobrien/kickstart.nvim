e776003715df60bce032483f98e82b3ab178403b
Sean O'Brien <seancanobrien@hotmail.co.uk>
2025-10-17 10:51:04 +0100

    Changed auto indentation for .tex files in vimtex

--- lua/custom/plugins/vimtex.lua
+++ lua/custom/plugins/vimtex.lua
@@ -6,11 +6,21 @@ return {
     init = function()
       -- makes the quickfix window go away after any keystrokes
       vim.g.vimtex_quickfix_autoclose_after_keystrokes = 1
+      -- vim.g.vimtex_indent_enabled = 1
       vim.g.vimtex_view_method = 'zathura_simple'
       vim.g.vimtex_compiler_latexmk = {
         -- Directory for output pdf and auxilary files
         out_dir = 'build',
       }
+      -- auto indent every save
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        pattern = '*.tex',
+        callback = function()
+          local view = vim.fn.winsaveview()
+          vim.cmd 'keepjumps normal! gg=G'
+          vim.fn.winrestview(view)
+        end,
+      })
     end,
   },
 }
